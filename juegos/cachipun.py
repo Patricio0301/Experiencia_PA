e31d5547d2b38456fc5829f76c3a38fa849a8407
vsepulvedat <163419925+vsepulvedat@users.noreply.github.com>
2024-03-14 21:08:30 +0000

    agregeue texto de resultado

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -4,5 +4,14 @@ def cachipun():
   while a!="piedra" and a!="papel" and a!="tijeras":
     print("Inválido, intente de nuevo")
     a=input().lower()
-  b=["piedra","papel","tijeras"];print(f"RESULTADO JUGADOR:\n{a}\nRESULTADO MÁQINA:\n{choice(b)}")
+  b = choice(["piedra","papel","tijeras"])
+  print(f"RESULTADO JUGADOR:\n{a}\nRESULTADO MÁQINA:\n{b}")
+  if a == b:
+    print ('Empate')
+  elif (a == 'piedra' and b == 'tijeras') or (a == 'papel' and b == 'piedra') or (a == 'tijeras' and b == 'papel'):
+    print ('Ganaste!')
+  else:
+    print ('Perdiste :(')
+
+  
 
