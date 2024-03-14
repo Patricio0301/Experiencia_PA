a9a6f905961f29ec14e9d894e7b165b8c7e8e4d3
vsepulvedat <163419925+vsepulvedat@users.noreply.github.com>
2024-03-14 13:56:20 +0000

    arregle una tipografia

--- juegos/adivinar_numero.py
+++ juegos/adivinar_numero.py
@@ -1,7 +1,7 @@
 from random import randint
 def adivinar_numero():
     prediccion = input ('adivina un numero del uno al diez (en numeral)')
-    n = ranint (1,10)
+    n = randint (1,10)
     if prediccion == n:
         print (f'Correcto! adivinaste que el número es  {n}')
         return
