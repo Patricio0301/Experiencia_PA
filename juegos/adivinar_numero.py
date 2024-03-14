0b108417728590695c6ddddf05c7751dd9265ed7
vsepulvedat <163419925+vsepulvedat@users.noreply.github.com>
2024-03-14 14:10:52 +0000

    arregle el print

--- juegos/adivinar_numero.py
+++ juegos/adivinar_numero.py
@@ -1,8 +1,8 @@
 from random import randint
 def adivinar_numero():
-    prediccion = input ('adivina un numero del uno al diez (en numeral):')
+    prediccion = input ('adivina un numero del uno al diez (en numeral) :')
     n = randint (1,10)
     if prediccion == str(n):
         print (f'Correcto! adivinaste que el número es  {n}')
         return
-    print (f'No :( el número es {n})')
+    print (f'No :( el número es {n}')
